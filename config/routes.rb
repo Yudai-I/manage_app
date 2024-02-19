19b19d6c50a9b848ac97c7599b25ffa01bfcc75d
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-20 02:19:15 +0900

    勤怠管理用のルーティング設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    post 'start_time' => 'attendances#start_time'
+    post 'end_time' => 'attendances#end_time'
+  end
 end
