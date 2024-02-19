bb56e61f820109673414be456e68e11f5c52dbc8
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-20 02:11:27 +0900

    勤怠管理に必要なコントローラと必要なメソッドの準備完了

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,12 @@
+class AttendancesController < ApplicationController
+    def start_time
+    end
+
+    def end_time
+    end
+
+    private
+    def time_params
+      params.require(:attendance).permit(:start_time, :end_time, :status)
+    end
+end
