28820301eb3eee2571e1764bc4d781e898ce77fc
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-22 18:15:23 +0900

    管理者によって作成されたシフトの情報をユーザーが確認できる機能を実装 & 合わせて変数名も軽く修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,6 +6,7 @@ class AttendancesController < ApplicationController
       @year = params[:year].to_i
       @month = params[:month].to_i
       @get_attendance_info = get_attendance_info(year, month)
+      @get_shift_info = get_shift_info(year, month)
     end
 
     def work
@@ -77,5 +78,31 @@ class AttendancesController < ApplicationController
         { date: date, day_of_week: formatting_week(date.strftime("%A")), start_time: start_time, end_time: end_time }
       end
       return get_attendance_info
+    end
+
+    def get_shift_info(year, month)
+      if year == 0 and month == 0
+          year = Date.current.year
+          month = Date.current.month
+      end
+      start_date = Time.new(year, month, 1).beginning_of_month
+      end_date = Time.new(year, month, 1).end_of_month
+      get_shift_info = (start_date.to_date..end_date.to_date).map do |date|
+        day = date.day
+        # shift = Shift.where(year: year).where(month: month).where(day: day).where(user_id: current_user.id)
+        shift = Shift.find_by(year: year, month: month, day: day, user_id: current_user.id)
+        if shift.present?
+          start_time_hour = shift.start_time_hour.to_s
+          end_time_hour = shift.end_time_hour.to_s
+          # 分の数が1桁の場合に、2桁目に0を補充するように
+          start_time_minute = sprintf("%02d", shift.start_time_minute)
+          end_time_minute = sprintf("%02d", shift.end_time_minute)
+          start_time = "#{start_time_hour}:#{start_time_minute}"
+          end_time = "#{end_time_hour}:#{end_time_minute}"
+        end
+
+        { date: date, day_of_week: formatting_week(date.strftime("%A")), start_time: start_time, end_time: end_time }
+      end
+      return get_shift_info
     end
 end
