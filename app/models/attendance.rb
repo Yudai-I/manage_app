111cc206db822f7c3617bff3872ed92332b424b5
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-24 18:07:26 +0900

    タスクの残件数を表示できる機能の実装完了

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,4 +4,8 @@ class Attendance < ApplicationRecord
     def self.get_todo_info(date, user_id)
       todos = Todo.where(date: date, user_id: user_id)
     end
+
+    def self.count_task_is_completed_false(todos)
+      todos.select { |todo| !todo.is_completed }.count
+    end
 end
