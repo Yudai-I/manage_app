2b185ad0de85ee967a2cc70180a55c647ddfab1f
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2024-02-25 23:49:51 +0900

    マイページで先月働いた時間が表示される機能の実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def show
+    start_date = 1.month.ago.beginning_of_month
+    end_date = 1.month.ago.end_of_month
     
+    attendances = Attendance.where(user_id: current_user.id, status: false, start_time: start_date..end_date)
+    work_duration_sec_last_month = 0
+    # 先月の総勤務時間を秒単位で取得
+    attendances.each do |attendance|
+      work_duration_sec_last_month += attendance.end_time - attendance.start_time
+    end
+    @work_duration_hours_last_month = (work_duration_sec_last_month / 3600).round(1)
+
   end
 end
